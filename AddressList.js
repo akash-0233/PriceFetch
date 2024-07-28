f625b6fbcc69827f3e1b0f53b792af777af7be9c
akash-0233 <akashrajput0233@gmail.com>
2024-07-28 20:49:21 +0530

    Setup file structure and install Ether.js

---
+++ AddressList.js
@@ -0,0 +1,11 @@
+const factoryAddresss = "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73"; //factory address pancakeswap bnb smart chain explorer
+const routerAddress = "0x10ED43C718714eb63d5aA57B78B54704E256024E"; //router contract pancake address
+const fromAddress = "0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56"; //BUSD
+const toAddress = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"; //WBNB
+
+module.exports = {
+  factoryAddresss,
+  routerAddress,
+  fromAddress,
+  toAddress,
+};
